a08809c0f722e65483ce26590ae7f5aaadec62eb
Anna Tolstova <anna_tolstova@hotmail.com>
2022-06-05 19:49:44 +0300

    feat: add fs:create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,21 @@
+import { writeFile } from 'fs';
+import { resolvePath } from './resolvePath.js';
+
 export const create = async () => {
-    // Write your code here 
+    const filePath = resolvePath("fresh.txt");
+    const fileData = "I am fresh and young"; 
+
+    writeFile(filePath, fileData, 
+        { encoding: 'utf8', flag: 'wx+'}, (err, data) => {
+        try {
+            if (err) throw new Error("FS operation failed");
+            
+            console.log(data);
+        }
+        catch (e) {
+            console.error(e.message);
+        }
+    });
 };
+
+await create();
