edb64b0e4929237dd0603dddd24334d55dfff80f
Anna Tolstova <anna_tolstova@hotmail.com>
2022-06-05 15:40:53 +0300

    feat: add fs:read solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,23 @@
+import * as path from 'path';
+import { readFile } from 'fs';
+import { fileURLToPath } from 'url';
+
 export const read = async () => {
-    // Write your code here 
+    const dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filesDirPath = path.join(dirname, './files');
+
+    const filePath = path.resolve(filesDirPath, "fileToRead.txt");    
+
+    readFile(filePath, 'utf8', (err, data) => {
+        try {
+            if (err) throw new Error("FS operation failed");
+            
+            console.log(data);
+        }
+        catch (e) {
+            console.error(e.message);
+        }
+    });
 };
+
+await read();
